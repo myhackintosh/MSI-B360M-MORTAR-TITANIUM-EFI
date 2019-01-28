aeb46e1030665c7f0ebc33856da8d6b9facbd908
梁志峰 <liangzhifeng@xunlei.com>
2019-01-28 10:57:45 +0800

    update Clover to 4862; use AptioFix-R24-RELEASE of AptioMemoryFix-64.efi to fix startup stability of 10.14.3

--- update_clover.sh
+++ update_clover.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-CLOVER=/Volumes/Clover-v2.4k-4844-X64
+set -x
+CLOVER=/Volumes/Clover-v2.4k-4862-X64
+if [ ! -d $CLOVER ]; then 
+   echo "$CLOVER not exists!"
+   exit 1
+fi
+
 cp -pR $CLOVER /tmp/Clover
 CLOVER=/tmp/Clover
 
@@ -7,21 +13,23 @@ cp -pR $CLOVER/EFI/CLOVER/drivers-Off/drivers64UEFI/* $CLOVER/EFI/CLOVER/drivers
 
 EFI=`pwd`
 
+# we use AptioMemoryFix.efi of github Release version!
 file_list=(
 EFI/BOOT/BOOTX64.efi 
-EFI/CLOVER/CLOVERX64.efi 
-EFI/CLOVER/drivers64UEFI/ApfsDriverLoader-64.efi 
-EFI/CLOVER/drivers64UEFI/AptioMemoryFix-64.efi 
-EFI/CLOVER/drivers64UEFI/FSInject-64.efi 
-EFI/CLOVER/drivers64UEFI/SMCHelper-64.efi 
+EFI/CLOVER/CLOVERX64.efi
+EFI/CLOVER/drivers64UEFI/SMCHelper-64.efi
+EFI/CLOVER/drivers64UEFI/ApfsDriverLoader-64.efi
+EFI/CLOVER/drivers64UEFI/FSInject-64.efi
+EFI/CLOVER/drivers64UEFI/EmuVariableUefi-64.efi
 )
 
 for filename in ${file_list[@]}
 do 
-	cp -p $CLOVER/$filename $EFI/$filename
+    cp -p $CLOVER/$filename $EFI/$filename
 done
 
-cp -pR $CLOVER/EFI/CLOVER/drivers-Off/* $EFI/EFI/CLOVER/drivers-Off
+# update drivers-Off
+cp -pR $CLOVER/EFI/CLOVER/drivers-Off/drivers64UEFI/* $EFI/EFI/CLOVER/drivers-Off/drivers64UEFI/
 cp -pR $CLOVER/EFI/CLOVER/tools/*       $EFI/EFI/CLOVER/tools
 
 rm -rf $CLOVER
